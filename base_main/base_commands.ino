bfdf060448ad8660e87000fb58dd9293428aa1db
harrisong66 <harrisongallo66@gmail.com>
2026-05-02 23:18:11 -0500

    Update base_commands.ino

--- base_main/base_commands.ino
+++ base_main/base_commands.ino
@@ -1,9 +1,9 @@
-const int NODE_PINS[3] = {2, 6, 7}; // Map your indices 0, 1, 2
+const int NODE_PINS[3] = {2, 6, 7}; // Index 0: Node_L, Index 1: Node_R, Index 2: NONE (Center)
 
 void setupPins() {
   for (int i = 0; i < 3; i++) {
     pinMode(NODE_PINS[i], OUTPUT);
-    digitalWrite(NODE_PINS[i], HIGH); // OFF
+    digitalWrite(NODE_PINS[i], HIGH); // Default OFF
   }
 }
 
@@ -12,27 +12,22 @@ void triggerTotalShutdown() {
 }
 
 void executeCommand(String node, String value) {
-  // Determine the target signal (High=LOW, Low=HIGH due to Active Low relay)
-  int signal = (value == "High") ? LOW : HIGH;
+  int signal = (value == "High") ? LOW : HIGH; // Active Low logic
 
-  // Check for Global "ALL" Command
   if (node == "ALL") {
-    Serial.println("Executing command on ALL nodes.");
-    for (int i = 0; i < 3; i++) {
-      digitalWrite(NODE_PINS[i], signal);
-    }
+    for (int i = 0; i < 3; i++) digitalWrite(NODE_PINS[i], signal);
   } 
-  // Standard Individual Node Mapping
   else {
     int pinIdx = -1;
+    // Updated Mapping
     if (node == "Node_L") pinIdx = 0;
     else if (node == "Node_R") pinIdx = 1;
-    else if (node == "Node_M") pinIdx = 2;
+    else if (node == "NONE") pinIdx = 2; // Maps center node to pin index 2
 
     if (pinIdx != -1) {
       digitalWrite(NODE_PINS[pinIdx], signal);
     } else {
-      Serial.println("Error: Unknown Node identifier.");
+      Serial.println("Error: Unknown Node identifier: " + node);
     }
   }
 }
