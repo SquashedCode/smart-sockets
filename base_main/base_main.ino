b432b2c35be17d814ef0e3b938fc4c2963f35d1a
harrisong66 <harrisongallo66@gmail.com>
2026-05-02 23:13:35 -0500

    Update base_main.ino

--- base_main/base_main.ino
+++ base_main/base_main.ino
@@ -6,33 +6,24 @@
 const String BASE_NAME = "Base_1";
 const int UDP_PORT = 50000;
 unsigned long lastPacketReceived = millis();
+unsigned long lastHeartbeatSent = 0; 
 bool isShutdown = false;
 
 WiFiUDP udp;
 
 void setup() {
   Serial.begin(115200);
-  setupPins(); // From base_commands
+  setupPins(); 
   
   WiFi.begin("CrimsonTraveler-2.4", "3CrimsonCrows!");
   while (WiFi.status() != WL_CONNECTED) { delay(500); }
   
-  // Initialize NTP for accurate timestamps
-  configTime(0, 0, "pool.ntp.org"); 
-  
+  configTime(0, 0, "pool.ntp.org");
   udp.begin(UDP_PORT);
 }
 
-// 
-String getFormattedTime() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) return "0-0-0-0:0:0";
-  char buffer[32];
-  strftime(buffer, sizeof(buffer), "%m-%d-%y-%H:%M:%S", &timeinfo);
-  return String(buffer);
-}
-
 void loop() {
+  // Receive UDP
   int packetSize = udp.parsePacket();
   if (packetSize > 0) {
     char incoming[512];
@@ -41,10 +32,16 @@ void loop() {
     handleIncomingUDP(incoming, udp.remoteIP(), udp.remotePort());
   }
 
+  // Periodic ESP-to-PI Heartbeat
+  if (millis() - lastHeartbeatSent > 3000) {
+    sendHeartbeat();
+    lastHeartbeatSent = millis();
+  }
+
   // FR3: Safe Mode Timeout (6s)
   if (!isShutdown && (millis() - lastPacketReceived > 6000)) {
     Serial.println("Heartbeat lost. Safe Mode engaged.");
-    triggerTotalShutdown();
+    triggerTotalShutdown(); //
     isShutdown = true;
   }
 }
