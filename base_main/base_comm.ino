f3717e10ba5e6a4eb416cfb458aae3356ba0fbb7
harrisong66 <harrisongallo66@gmail.com>
2026-05-04 07:05:41 -0500

    Update base_comm.ino

--- base_main/base_comm.ino
+++ base_main/base_comm.ino
@@ -58,11 +58,12 @@ void processIncomingUDP(char* rawData, IPAddress senderIP, int senderPort) {
   }
   
   else if (isDiscovered && !isShutdown && action == "power") {
+    String cmdID = doc["command_id"] | "0";
     String targetNode = doc["node"] | "";
     String val = doc["value"] | "low";
     
     executeCommand(targetNode, val);
-    sendCommandResponse(senderIP, senderPort);
+    sendCommandResponse(cmdID, senderIP, senderPort);
   }
 }
 // DISCOVERY RESPONSE (ESP -> PI)
@@ -73,30 +74,31 @@ void sendDiscoveryResponse(IPAddress ip, int port) {
   doc["action"] = "discovery_response"; // Ensure this key matches your Pi's expected key
 
   // Node_L
-  JsonObject node_L = doc.createNestedObject("Node_L");
-  node_L["attached"] = getAttachmentStatus(isNodeAttached(0));
-  node_L["power"] = getPowerStatus(isNodeOn(0));
+  JsonObject node_l = doc.createNestedObject("Node_L");
+  node_l["attached"] = getAttachmentStatus(isNodeAttached(0));
+  node_l["power"] = getPowerStatus(isNodeOn(0));
   
   // Node_R
-  JsonObject node_R = doc.createNestedObject("Node_R");
-  node_R["attached"] = getAttachmentStatus(isNodeAttached(1));
-  node_R["power"] = getPowerStatus(isNodeOn(1));
+  JsonObject node_r = doc.createNestedObject("Node_R");
+  node_r["attached"] = getAttachmentStatus(isNodeAttached(1));
+  node_r["power"] = getPowerStatus(isNodeOn(1));
 
   // Node_C
-  JsonObject node_C = doc.createNestedObject("Node_C");
-  node_C["power"] = getPowerStatus(isNodeOn(2));
+  JsonObject node_c = doc.createNestedObject("Node_C");
+  node_c["power"] = getPowerStatus(isNodeOn(2));
 
   // Send the packet
-  sendJsonPacket(doc, ip, port, "Discovery_Response");
+  sendJsonPacket(doc, ip, port, "discovery_response");
 }
 
 // COMMAND RESPONSE (ESP -> PI)
-void sendCommandResponse(IPAddress ip, int port) {
-  StaticJsonDocument<256> doc;
-  doc["base_name"] = BASE_NAME;
-  doc["base_ip"] = WiFi.localIP().toString();
-  doc["status"] = "success";
-  doc["time"] = getFormattedTime();
+void sendCommandResponse(String cmdID, IPAddress ip, int port) {
+  StaticJsonDocument<512> doc;
+  // Header Info
+  doc["action"] = "command_response";
+  doc["command_id"] = cmdID;
+  doc["base"] = BASE_NAME;
+  doc["status"] = "successful";
   
   sendJsonPacket(doc, ip, port, "command_response");
 }
